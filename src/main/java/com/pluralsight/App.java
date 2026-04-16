430851062a6db59c98f1304ba24f6b2136ebaa06
yuujn <pale.cup5223@fastmail.com>
2026-04-16 11:56:05 -0500

    keep track of index instead of doing silly linear search

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class App {
     static Scanner scan = new Scanner(System.in);
+    static Vehicle[] vehicles = new Vehicle[20];
+    static int vehicleCount = 0;
 
     public static void main(String[] args) {
-        Vehicle[] vehicles = new Vehicle[20];
         vehicles[0] = new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500);
         vehicles[1] = new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000);
         vehicles[2] = new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700);
         vehicles[3] = new Vehicle(101124, "Honda Civic", "White", 70000, 7500);
         vehicles[4] = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500);
         vehicles[5] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000);
+        vehicleCount = 6;
 
         while (true) {
             showMenu();
@@ -22,19 +24,19 @@ public class App {
             }
             switch (choice) {
                 case 1:
-                    listAllVehicles(vehicles);
+                    listAllVehicles();
                     break;
                 case 2:
-                    searchByMakeModel(vehicles);
+                    searchByMakeModel();
                     break;
                 case 3:
-                    searchByPriceRange(vehicles);
+                    searchByPriceRange();
                     break;
                 case 4:
-                    searchByColor(vehicles);
+                    searchByColor();
                     break;
                 case 5:
-                    addVehicle(vehicles);
+                    addVehicle();
                     break;
                 default:
                     System.out.println("That menu option is not implemented yet. Sorry!");
@@ -56,7 +58,7 @@ public class App {
         System.out.print("Enter your command: ");
     }
 
-    static void listAllVehicles(Vehicle[] vehicles) {
+    static void listAllVehicles() {
         for (Vehicle vehicle : vehicles) {
             if (vehicle != null) {
                 System.out.println(vehicle);
@@ -64,7 +66,7 @@ public class App {
         }
     }
 
-    static void searchByMakeModel(Vehicle[] vehicles) {
+    static void searchByMakeModel() {
         System.out.print("Enter Make / Model: ");
         String makeModel = readLine();
         for (Vehicle vehicle : vehicles) {
@@ -75,7 +77,8 @@ public class App {
             }
         }
     }
-    static void searchByPriceRange(Vehicle[] vehicles) {
+
+    static void searchByPriceRange() {
         System.out.print("Enter Bottom of Price Range: ");
         float bottom = scan.nextFloat();
         System.out.print("Enter Top of Price Range: ");
@@ -88,7 +91,8 @@ public class App {
             }
         }
     }
-    static void searchByColor(Vehicle[] vehicles) {
+
+    static void searchByColor() {
         System.out.print("Enter Color: ");
         String color = readLine();
         for (Vehicle vehicle : vehicles) {
@@ -100,7 +104,7 @@ public class App {
         }
     }
 
-    static void addVehicle(Vehicle[] vehicles) {
+    static void addVehicle() {
         System.out.print("Enter a vehicle ID: ");
         long id = scan.nextLong();
         System.out.print("Enter a make / model: ");
@@ -120,13 +124,11 @@ public class App {
                 price
         );
 
-        for (int i = 0; i < vehicles.length; i++) {
-            if (vehicles[i] == null) {
-                vehicles[i] = newVehicle;
-                return;
-            }
+        if (vehicleCount < vehicles.length) {
+            vehicles[vehicleCount++] = newVehicle;
+        } else {
+            System.out.println("Failed to add vehicle. Out of available slots.");
         }
-        System.out.println("Failed to add vehicle. Out of available slots.");
     }
 
     static String readLine() {
